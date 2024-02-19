4a37e4accb3084a6ab6e65db91177c964a79f705
orekesh <zizikesh17@gmail.com>
2024-02-19 00:42:22 -0500

    initial commit

---
+++ variable.tf
@@ -0,0 +1,27 @@
+
+variable "endpoint" {
+  type    = string
+  default = ""
+}
+
+variable "instanceid" {
+  type    = string
+  default = ""
+}
+
+variable "cidr_blocks" {
+  type    = string
+  default = ""
+}
+
+variable "ipv6_cidr_blocks" {
+  type    = string
+  default = ""
+}
+
+variable "kms_id" {
+  type    = string
+  default = ""
+
+}
+
